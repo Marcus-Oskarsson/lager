8611c082f6eff333faaa8fc0b2ce497c0a4171fa
Marcus <marcusoskarsson@hotmail.com>
2022-04-02 11:53:41 +0100

    update image and set font properties

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Stock from "./components/Stock";
-import warehouse from "./assets/warehouse.jpg";
+import bookcases from "./assets/bookcases.jpg";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.base}>
-        <Text style={styles.text}>Lager-Appen</Text>
-        <Image source={warehouse} style={{ width: 320, height: 240 }} />
+      <ScrollView style={styles.base}>
+        <Text style={styles.text}>Boklager för webapp</Text>
+        <Image source={bookcases} style={styles.image} />
         <Stock />
-        <StatusBar style="auto" />
-      </View>
+        <StatusBar style="light" />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -21,15 +21,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#000",
+  },
+  image: {
+    width: 320,
+    height: 240,
   },
   base: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#000",
+    padding: 16,
   },
   text: {
-    color: "#33c",
-    fontSize: 42,
+    color: "#efefef",
+    fontSize: 24,
+    marginBottom: 32,
+    marginTop: 32,
   },
 });
