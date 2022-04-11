64543ae8ba8aa08cb8c755860ed47eb82d95b6b3
Marcus <marcusoskarsson@hotmail.com>
2022-04-11 15:44:32 +0100

    seperate styling

---
+++ styles/base.js
@@ -0,0 +1,35 @@
+export const container = {
+  flex: 1,
+  backgroundColor: "#000",
+};
+
+export const image = {
+  width: 320,
+  height: 240,
+  marginBottom: 28,
+};
+
+export const base = {
+  flex: 1,
+  backgroundColor: "#000",
+  padding: 16,
+};
+
+export const paddingBottom = {
+  paddingBottom: 28,
+};
+
+export const button = {
+  color: "efefef",
+  backgroundColor: "#24a0ed",
+  padding: 12,
+  marginBottom: 24,
+  alignItems: "center",
+};
+
+export const buttonDisable = {
+  backgroundColor: "#ff0000",
+  padding: 12,
+  marginBottom: 24,
+  alignItems: "center",
+};
---
+++ styles/typography.js
@@ -0,0 +1,32 @@
+export const header1 = {
+  fontSize: 42,
+  marginBottom: 28,
+};
+
+export const header2 = {
+  fontSize: 34,
+  marginBottom: 28,
+};
+
+export const header3 = {
+  fontSize: 28,
+  marginBottom: 28,
+};
+
+export const normal = {
+  fontSize: 20,
+  marginBottom: 28,
+};
+
+export const normalTextColor = {
+  color: "#efefef",
+};
+
+export const header2color = {
+  color: "#1f2484",
+  fontWeight: "bold",
+};
+
+export const button = {
+  fontSize: 24,
+};
