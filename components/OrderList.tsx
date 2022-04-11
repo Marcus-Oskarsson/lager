6b9f565516e5732496a453d8c34925e21c2db751
Marcus <marcusoskarsson@hotmail.com>
2022-04-11 15:41:42 +0100

    showing orders and picking order to process

---
+++ components/OrderList.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import orderModel from "../models/orders";
+import { Base, Typography } from "../styles";
+
+const OrderList = ({ route, navigation }) => {
+  let { reload } = route.params || false;
+  const [allOrders, setAllOrders] = useState([]);
+
+  async function reloadOrders() {
+    setAllOrders(await orderModel.getOrders());
+  }
+
+  if (reload) {
+    reloadOrders();
+  }
+
+  useEffect(() => {
+    reloadOrders();
+  }, []);
+
+  const listOfOrders = allOrders
+    .filter((order) => order.status === "Ny")
+    .map((order, index) => {
+      return (
+        <TouchableOpacity
+          style={Base.button}
+          key={index}
+          onPress={() => {
+            navigation.navigate("Details", {
+              order: order,
+            });
+          }}
+        >
+          <Text style={{ ...Typography.normalTextColor, ...Typography.button }}>
+            {order.name}
+          </Text>
+        </TouchableOpacity>
+      );
+    });
+  return (
+    <View style={Base.base}>
+      <Text style={{ ...Typography.header3, ...Typography.normalTextColor }}>
+        Alla ordrar
+      </Text>
+      {listOfOrders}
+    </View>
+  );
+};
+
+export default OrderList;
---
+++ components/PickList.tsx
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import orderModel from "../models/orders";
+import productModel from "../models/products";
+
+import { Base, Typography } from "../styles";
+
+export default function PickList({ route, navigation, setProducts }) {
+  const { order } = route.params;
+
+  useEffect(() => {
+    async () => {
+      setProducts(await productModel.getProducts());
+    };
+  }, []);
+
+  async function pick() {
+    await orderModel.pickOrder(order);
+    navigation.navigate("List", { reload: true });
+  }
+
+  const orderItemsList = order.order_items.map((item, index) => {
+    return (
+      <Text
+        key={index}
+        style={{ ...Typography.normal, ...Typography.normalTextColor }}
+      >
+        {item.name}
+        {"\n"}Antal: {item.amount}
+        {"\n"}lager: {item.stock} {"\n"}Hylla: {item.location}
+      </Text>
+    );
+  });
+
+  let inStock = true;
+  for (let i = 0; i < order.order_items.length; i++) {
+    if (order.order_items[i].amount > order.order_items[i].stock) {
+      inStock = false;
+    }
+  }
+
+  const btnPick = (
+    <TouchableOpacity style={Base.button} onPress={pick}>
+      <Text style={{ ...Typography.normalTextColor, ...Typography.button }}>
+        Plocka order
+      </Text>
+    </TouchableOpacity>
+  );
+  const btnNotInStock = (
+    <TouchableOpacity style={Base.buttonDisable} onPress={pick} disabled>
+      <Text style={{ ...Typography.normalTextColor, ...Typography.button }}>
+        Ej i lager
+      </Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={Base.base}>
+      <Text style={{ ...Typography.normal, ...Typography.normalTextColor }}>
+        {order.name}
+      </Text>
+      <Text style={{ ...Typography.normal, ...Typography.normalTextColor }}>
+        {order.address}
+      </Text>
+      <Text style={{ ...Typography.normal, ...Typography.normalTextColor }}>
+        {order.zip} {order.city}
+      </Text>
+
+      <Text style={{ ...Typography.header2, ...Typography.header2color }}>
+        Produkter:
+      </Text>
+
+      {orderItemsList}
+      {inStock ? btnPick : btnNotInStock}
+    </View>
+  );
+}
