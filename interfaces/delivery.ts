817898ac18ede48f1b7b6145d70fd786530ef426
Marcus <marcusoskarsson@hotmail.com>
2022-04-14 21:50:38 +0100

    delivery interface

---
+++ interfaces/delivery.ts
@@ -0,0 +1,11 @@
+import Specifiers from "./specifiers";
+
+interface Delivery {
+  product_id: number;
+  amount: number;
+  product_name: string;
+  delivery_date: string;
+  comment: string;
+}
+
+export default Delivery;
