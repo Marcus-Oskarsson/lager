333b841f6ccc0d9c3b98aba6f95c5b2063007dd3
marcus-oskarsson <marcusoskarsson@hotmail.com>
2022-04-18 11:33:26 +0200

    styling for forms

---
+++ styles/forms.js
@@ -0,0 +1,12 @@
+export const label = {
+  color: "red",
+};
+
+export const input = {
+  fontSize: 20,
+  marginBottom: 28,
+  borderWidth: 1,
+  padding: 10,
+  borderColor: "#ccc",
+  borderRadius: 3,
+};
