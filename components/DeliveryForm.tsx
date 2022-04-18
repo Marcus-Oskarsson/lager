60b7e5f33ba557d727b54b265c30eba11d4a0adf
Marcus <marcusoskarsson@hotmail.com>
2022-04-18 20:31:41 +0100

    impement complete form for deliveries

--- components/DeliveryForm.tsx
+++ components/DeliveryForm.tsx
@@ -10,27 +10,67 @@ import { Base, Typography, Forms } from "../styles";
 
 import Delivery from "../interfaces/Delivery";
 import Product from "../interfaces/product";
+import productModel from "../models/products";
+import deliveryModel from "../models/delivery";
+
 import ProductDropDown from "./ProductDropDown";
+import DateDropDown from "./DateDropDown";
 
 const DeliveryForm = ({ navigation }) => {
   const [delivery, setDelivery] = useState<Partial<Delivery>>({});
   const [currentProduct, setCurrentProduct] = useState<Partial<Product>>({});
+  const [validDelivery, setValidDelivery] = useState<boolean>(false);
+
+  const checkDelivery = () => {
+    if (
+      delivery.product_id !== undefined &&
+      delivery.delivery_date !== undefined &&
+      delivery.amount !== undefined
+    ) {
+      setValidDelivery(true);
+    }
+  };
 
   const handleCommentUpdate = (text: string) => {
-    // Testar såhär istället
-    console.log(text);
     setDelivery({ ...delivery, comment: text });
+    checkDelivery();
   };
 
   const handleAmountUpdate = (x: string) => {
-    // Testar såhär istället
-    console.log(x);
     setDelivery({ ...delivery, amount: parseInt(x) });
+    checkDelivery();
   };
 
-  const addDelivery = () => {
-    console.log(delivery);
+  const addDelivery = async () => {
+    await deliveryModel.addDelivery(delivery);
+    const updatedProduct = {
+      ...currentProduct,
+      stock: (currentProduct.stock || 0) + (delivery.amount || 0),
+    };
+
+    await productModel.updateStock(updatedProduct);
+
+    navigation.navigate("List", { reload: true });
   };
+
+  const buttonDeliveryValid = (
+    <TouchableOpacity onPress={addDelivery}>
+      <View style={Base.buttonValid}>
+        <Text style={{ ...Typography.normalTextColor, ...Typography.button }}>
+          Lägg till inleverans
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+  const buttonDeliveryInvalid = (
+    <TouchableOpacity onPress={addDelivery} disabled>
+      <View style={Base.buttonDisable}>
+        <Text style={{ ...Typography.normalTextColor, ...Typography.button }}>
+          Saknas information
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <ScrollView style={{ ...Base.base }}>
@@ -44,6 +84,7 @@ const DeliveryForm = ({ navigation }) => {
         style={{ ...Forms.input, ...Typography.normalTextColor }}
         onChangeText={handleCommentUpdate}
         value={delivery?.comment}
+        maxLength={140}
       />
       <Text style={{ ...Typography.label, ...Typography.normalTextColor }}>
         Antal
@@ -51,7 +92,7 @@ const DeliveryForm = ({ navigation }) => {
       <TextInput
         style={{ ...Forms.input, ...Typography.normalTextColor }}
         onChangeText={handleAmountUpdate}
-        value={delivery?.amount?.toString()}
+        value={delivery?.amount?.toString() ? delivery.amount.toString() : "0"}
         keyboardType="numeric"
       />
       <Text style={{ ...Typography.label, ...Typography.normalTextColor }}>
@@ -61,19 +102,19 @@ const DeliveryForm = ({ navigation }) => {
         delivery={delivery}
         setDelivery={setDelivery}
         setCurrentProduct={setCurrentProduct}
+        checkDelivery={checkDelivery}
       />
-      <TouchableOpacity onPress={addDelivery}>
-        {/* Borde kolla om all information som behövs finns med! */}
-        <View style={Base.button}>
-          <Text style={{ ...Typography.normalTextColor, ...Typography.button }}>
-            Lägg till inleverans
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <Text style={{ ...Typography.label, ...Typography.normalTextColor }}>
+        Datum för inleverans
+      </Text>
+      <DateDropDown
+        delivery={delivery}
+        setDelivery={setDelivery}
+        checkDelivery={checkDelivery}
+      />
+      {validDelivery ? buttonDeliveryValid : buttonDeliveryInvalid}
     </ScrollView>
   );
 };
 
 export default DeliveryForm;
-
-// Kom ihåg att ändra alla knappar så de har en view i sej!
